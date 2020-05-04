e31162ed7b14199cdb8019fa6b8f1662133d7b45
Dios Vo <vtmn1212@gmail.com>
2020-05-04 22:51:52 +0700

    create signup-page

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NewListComponent } from './pages/new-list/new-list.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 
 const routes: Routes = [
@@ -12,8 +13,8 @@ const routes: Routes = [
   { path: 'lists', component: TaskViewComponent },
   { path: 'lists/:listId', component: TaskViewComponent },
   { path: 'lists/:listId/new-task', component: NewTaskComponent },
-  { path: 'login', component: LoginPageComponent }
-
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent }
 ];
 
 @NgModule({
