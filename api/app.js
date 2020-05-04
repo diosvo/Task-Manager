7ef93d100944c290cd46541670886d03e0da38b4
Dios Vo <vtmn1212@gmail.com>
2020-05-04 22:33:08 +0700

    Handling Access Token Refresh

--- api/app.js
+++ api/app.js
@@ -18,9 +18,8 @@ app.use(bodyParser.json());
 // CORS HEADER MIDDLEWARE
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Methods", "HEAD, PUT, PATCH, DELETE, POST, GET, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
     res.header(
         'Access-Control-Expose-Headers',
         'x-access-token, x-refresh-token'
@@ -269,7 +268,7 @@ app.delete('/lists/:listId/tasks/:taskId', authenticate, (req, res) => {
 });
 /* === END TASK ROUTES === */
 
-/** USER ROUTE */
+/** === USER ROUTE === */
 
 /**
  * POST /users
--- ui/src/app/web-req.interceptor.ts
+++ ui/src/app/web-req.interceptor.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, throwError, empty } from 'rxjs';
+import { catchError, tap, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,8 @@ import { catchError } from 'rxjs/operators';
 export class WebReqInterceptor implements HttpInterceptor {
 
   constructor(private authService: AuthService) { }
+
+  refreshingAccessToken: boolean;
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
     request = this.addAuthHeader(request);
@@ -19,7 +21,34 @@ export class WebReqInterceptor implements HttpInterceptor {
       catchError((error: HttpErrorResponse) => {
         console.log(error);
 
+        if (error.status === 401 && this.refreshingAccessToken) {
+          // Unauthorized
+          // Refresh the access token
+          return this.refreshAccessToken()
+            .pipe(
+              switchMap(() => {
+                request = this.addAuthHeader(request);
+                return next.handle(request);
+              }),
+              catchError((err: any) => {
+                console.log(err);
+                this.authService.logout();
+                return empty();
+              })
+            )
+        }
         return throwError(error);
+      })
+    )
+  }
+
+  refreshAccessToken() {
+    this.refreshingAccessToken = true;
+    // Call method in auth service
+    return this.authService.getNewAccessToken().pipe(
+      tap(() => {
+        this.refreshingAccessToken = false;
+        console.log('Access Token refreshed');
       })
     )
   }
